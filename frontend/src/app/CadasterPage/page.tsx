f6d2d85a9c36ed6e40de847d1895842cb371d1a2
Tobias Perassi Alquezar <tobiasperassi@gmail.com>
2025-05-20 12:20:54 -0300

    :sparkles: feat: Creating the loading page

---
+++ frontend/src/app/CadasterPage/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+export default function CadasterPage() {
+ 
+
+  return (
+    <div className="h-screen flex flex-col items-center justify-center">
+        
+    </div>
+  );
+}
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import LoadingPage from "@/components/carregando";
+
+export default function Home() {
+  return (
+    <div>
+      <LoadingPage />
+    </div>
+  );
+}
